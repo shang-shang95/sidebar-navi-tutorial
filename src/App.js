60fcbafde807719dd206d91c39cb6806b2635b28
shang-shang95 <qqxn7ra9k@gmail.com>
2023-09-18 16:09:11 +0900

    Add sidebar component with icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return <div className="App">こんにちは</div>;
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SidebarIcon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Icon from "../images/icon.jpeg";
+
+function SidebarIcon() {
+  return (
+    <div className="SidebarIcon">
+      <img src={Icon} alt="profile" />
+      <p>programming@gmail.com</p>
+    </div>
+  );
+}
+
+export default SidebarIcon;
